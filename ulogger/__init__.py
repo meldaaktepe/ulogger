be3853292e53ce6148f88fe910f4a5e04bfba8c6
Hatice Melda Aktepe <amelda@sabanciuniv.edu>
2025-05-19 20:27:34 +0300

    ref | Pkg structure updated

--- ulogger/__init__.py
+++ ulogger/__init__.py
@@ -2,5 +2,6 @@ import os
 try:
     with open("./VERSION", "r", encoding="utf-8") as version_file:
         __version__ = version_file.read().strip()
+        print(__version__)
 except FileNotFoundError as ex:
     print("File VERSION does not exist.")
